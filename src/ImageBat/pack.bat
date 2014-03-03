e02a700c648347a790ec41fc4b89f23d46ff064d
albumdown <albumdown@hotmail.com>
2014-03-03 03:22:35 +0000

    delete ImageBat

--- src/ImageBat/pack.bat
+++
@@ -1,6 +0,0 @@
-set nowpath=%~dp0 
-set rar="C:\Program Files\WinRAR\rar.exe"
- 
-set sTime=%date:~0,10%_
-
-%rar%  a %sTime%ImageBat *.rc *.sln *.vcproj *.bat *.h *.cpp *.txt res\*.* doc\*.* images\*.*
